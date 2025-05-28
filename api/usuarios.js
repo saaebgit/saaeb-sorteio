53e2f1f80ceac3cea04f7e5b46c3d1e92bd9c9cf
saaebgit <sti@saaeb.com.br>
2025-05-28 16:27:35 -0300

    proxy reverso post

--- api/usuarios.js
+++ api/usuarios.js
@@ -1,27 +1,19 @@
 export default async function handler(req, res) {
   const apiUrl = 'http://179.108.182.210:30001/usuarios/';
 
+  if (req.method !== 'POST') {
+    res.status(405).json({ error: 'Método não permitido. Use POST.' });
+    return;
+  }
+
   try {
-    const method = req.method;
-
-    let apiResponse;
-
-    if (method === 'GET') {
-
-      apiResponse = await fetch(apiUrl);
-    } else if (method === 'POST') {
-
-      apiResponse = await fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(req.body)
-      });
-    } else {
-      res.status(405).json({ error: 'Método não permitido' });
-      return;
-    }
+    const apiResponse = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(req.body)
+    });
 
     const data = await apiResponse.json();
     res.status(apiResponse.status).json(data);
